414940870d9b6d3b764d40e305587df2de2bf186
padalavenkatareddy <65186428+padalavenkatareddy@users.noreply.github.com>
2024-01-30 14:03:28 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull venkatreddy1999/simple-python-flask-app/general
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 venkatreddy1999/simple-python-flask-app/general
